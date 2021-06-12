395a7709d3fd2b025812e70e7ec6738cba176871
Khaya R. Dillon <yahyal0v3@yahoo.com>
2021-06-12 17:49:13 -0400

    Start App, Expenses, and ExpenseItem component

---
+++ src/components/Expenses.js
@@ -0,0 +1,14 @@
+import ExpenseItem from './ExpenseItem'
+
+const Expenses = (props) => {
+    return (
+        <div className="expenses"> 
+            <ExpenseItem title={props.expenses[0].title} date={props.expenses[0].date} price={props.expenses[0].amount} />
+            <ExpenseItem title={props.expenses[1].title} date={props.expenses[1].date} price={props.expenses[1].amount} />
+            <ExpenseItem title={props.expenses[2].title} date={props.expenses[2].date} price={props.expenses[2].amount} />
+        </div>
+      );
+
+}
+
+export default Expenses;
